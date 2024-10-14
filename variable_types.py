c4cfd26dca0818bc33f208b38a43dbe2da10df16
Avinash <avinashshiyani760@gmail.com>
2024-10-14 15:37:42 +0530

    types in python

---
+++ variable_types.py
@@ -0,0 +1,12 @@
+my_integer = 10
+my_float = 10.1
+my_complex = complex(3,5)
+my_string = "Hello world"
+my_boolean = True
+
+print(my_integer , my_float , my_complex , my_string , my_boolean)
+print(type(my_integer));
+print(type(my_float));
+print(type(my_complex));
+print(type(my_string));
+print(type(my_boolean));
