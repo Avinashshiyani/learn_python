4718449da0257245f67998dbba22008b09618620
Avinash <avinashshiyani760@gmail.com>
2024-10-15 17:21:34 +0530

    String and Methords of Strings in Py

---
+++ string.py
@@ -0,0 +1,17 @@
+# Create a string variable called `my_string` and assign it a sentence.
+# Print the length of `my_string`.
+# Convert `my_string` to all uppercase letters and print the result.
+# Replace a word in `my_string` with another word and print the modified string.
+my_string = "Hello my name is avinash"
+
+# length of string
+print(len(my_string))
+
+# upperCaseLetter 
+print(my_string.upper());
+
+# replace the string
+word = "shiyani avinash"
+
+replaced_string = my_string.replace("avinash" , word)
+print(replaced_string)
