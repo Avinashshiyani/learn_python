2e2c4768550e5f188e743a5d777256c198d92e1b
Avinash <avinashshiyani760@gmail.com>
2024-10-16 10:04:51 +0530

    Math liabrary to perform math in python

---
+++ math.py
@@ -0,0 +1,18 @@
+# Create two variables, n1 and n2, and assign them any numeric values.
+# Calculate the sum, difference, product, and division of n1 and n2 and print the results.
+# Calculate the square, cube and square root of n2 and print the results.
+# Calculate and display remainder of n1 and n2
+import math
+n1 = 10;
+n2 = 20;
+
+sumOfNumber = n1 + n2
+differenceOfNumber  = n2-n1
+productOfNumber = n1*n2
+divitionOfNumber = n1 / n2
+squareOfNumber = n2 * n2
+cubeOfNumber = n2*n2*n2
+squareRoot = math.sqrt(n2)
+remainderOfNumber = n1 % n2
+
+print(sumOfNumber , differenceOfNumber ,productOfNumber , divitionOfNumber , squareOfNumber , cubeOfNumber , squareRoot , remainderOfNumber)
