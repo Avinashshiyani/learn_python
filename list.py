3ca2ecce62656438eaab85a45f07cd1e3fb7f2cf
Avinash <avinashshiyani760@gmail.com>
2024-10-17 18:18:50 +0530

    List in python

---
+++ list.py
@@ -0,0 +1,15 @@
+# Create a list called `my_list` with at least five different elements (two integers, two
+# strings, one float).
+# Print the first element of the list.
+# Print the last element of the list.
+# Print a slice of the list that includes the second through fourth elements.
+my_list = [ 10 , 20 , "hello" , "world" , 10.20]
+
+# printfirstelement
+print(my_list[0])
+
+# lastelement
+print(my_list[-1])
+
+# sliceLlist
+print(my_list[2:4])
