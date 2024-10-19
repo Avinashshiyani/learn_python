3b1dc5247fa1b505cff673794165450ac5e89c59
Avinash <avinashshiyani760@gmail.com>
2024-10-19 10:45:46 +0530

    Dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+# 6
+# Create a dictionary called `my_dict` with at least three key-value pairs.
+# Access and print the values associated with one of the keys.
+# Add a new key-value pair to `my_dict`.
+# Remove one of the key-value pairs from `my_dict`.
+# Check if a specific key is in `my_dict`.
+my_dict = {
+    "name":"avinash",
+    "age":18,
+    "love":"coding",
+}
+# print(my_dict["love"])
+
+# added in tuple
+# my_dict["avi"] = "bro how are you"
+
+# remove in tuple
+# my_dict.pop("age")
+
+isThere = "avinash" in my_dict
+
+print(my_dict , isThere)
