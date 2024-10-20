ce3e0f386745e56beb84adc1af5548ae2d88a4de
Avinash <avinashshiyani760@gmail.com>
2024-10-20 10:02:01 +0530

    Data type conversation in python

---
+++ Main/dataTypeConversation.py
@@ -0,0 +1,18 @@
+# 7
+# Convert an integer to a string and print the result.
+# Convert a string containing a number to an integer and print the result.
+# Convert a float to an integer and print the result.
+# Convert a string to a list of characters and print the result.
+
+integer = 10 
+string = "10"
+toflo = 10.22
+toString = str(integer);
+# toInteger = int(string);
+toInteger = float(toflo);
+
+stringToChar = "Hello my name is HEllo world"
+
+a = list(stringToChar)
+
+print(len(a))
