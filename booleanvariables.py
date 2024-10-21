9e317ff35fccaa661c429eeac2b925032eee5b73
Avinash <avinashshiyani760@gmail.com>
2024-10-21 16:53:48 +0530

    Boolean and renames in py

---
+++ booleanvariables.py
@@ -0,0 +1,13 @@
+# 8
+# Create two boolean variables, `is_raining` and `is_sunny` and assign them values True or
+# False. Use boolean operators (and, or, not) to create a new variable ‘is_good_day’ that
+# represents whether it's a good day (e.g., not raining and sunny). Print the value of the new
+# variable ‘is_good_day’.
+
+is_raining = True
+is_sunny = False
+
+# is_good_day = is_raining or is_sunny
+is_good_day = is_raining and is_sunny
+# is_good_day = not is_raining and is_sunny
+print(is_good_day)
