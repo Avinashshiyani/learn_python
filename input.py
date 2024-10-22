d8bb22a9076596675b5e075d9f5a3c0da82e007b
Avinash <avinashshiyani760@gmail.com>
2024-10-22 12:58:45 +0530

    Input in py

---
+++ input.py
@@ -0,0 +1,9 @@
+# 9
+# Write a program that takes an integer as input and prints whether it is even or odd
+
+userInput = int(input("Enter number to check even or odd"))
+
+if (userInput % 2 == 0):
+    print("Number is Even")
+else:
+    print("Number is Odd")
