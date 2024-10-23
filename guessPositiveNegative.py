131deb6f950fc96baee3a71842fdcfa3186d257b
Avinash <avinashshiyani760@gmail.com>
2024-10-23 18:28:46 +0530

    Check Weather the number is positive or negative

---
+++ guessPositiveNegative.py
@@ -0,0 +1,11 @@
+# 10
+# Write a program that takes number and display whether it is positive, negative or zero.
+userInput = int(input("Enter number to check the result"))
+
+if(userInput > 0):
+    print("Positive")
+elif(userInput < 0):
+    print("negative")
+else:
+    print("Number is Zero")
+    
