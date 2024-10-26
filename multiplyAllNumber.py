076b9babc08410df4a3e62c342eebebcf440d395
Avinash <avinashshiyani760@gmail.com>
2024-10-26 16:00:39 +0530

    Multiply all number till maxnumber using loop

---
+++ multiplyAllNumber.py
@@ -0,0 +1,7 @@
+# 13
+# Write a program that prints the multiplication table of a given number
+number = 50;
+# i = range(1,11)
+
+for i in range(1,11):
+    print(number , "x" , i , "=" , number * i)
