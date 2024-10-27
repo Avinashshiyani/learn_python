653c8e1d7f1d899c7639ce22e494f3a1073c31a1
Avinash <avinashshiyani760@gmail.com>
2024-10-27 13:37:56 +0530

    Making a Multiply table from 1 to 10

---
+++ makeMultiplyTables.py
@@ -0,0 +1,5 @@
+# 14
+# Create a program that generates a multiplication table from 1 to 10 using nested loops.
+for i in range(1,11):
+    for j in range(1,11):
+        print(i ,"X" , j ,"=" , i * j)
