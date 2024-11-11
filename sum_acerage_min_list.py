ddf67a8775ad21391c9d59558daabca3b01b0350
Avinash <avinashshiyani760@gmail.com>
2024-11-11 09:52:01 +0530

    Sum average and mean in py

---
+++ sum_acerage_min_list.py
@@ -0,0 +1,18 @@
+# 8.
+# Write a python program to find the sum, average and minimum number from a list.
+
+number = [1 , 2 , 4 , 9 ,8, 7]
+
+# sum in list
+sumOfnumber = sum(number)
+print(sumOfnumber)
+
+# average of number
+averageNumber =  sumOfnumber / 6
+print(averageNumber)
+
+# minimum of number
+miniNumber = min(number)
+print(miniNumber)
+
+# print(number)
