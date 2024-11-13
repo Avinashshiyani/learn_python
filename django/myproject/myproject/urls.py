420b194ead923a3396973bce4b3ebcaed4e0722f
Avinash <avinashshiyani760@gmail.com>
2024-11-13 10:10:07 +0530

    Crud operation in django

--- django/myproject/myproject/urls.py
+++ django/myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for avinash project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+# from .models import Products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', include('Products.urls')),
 ]
