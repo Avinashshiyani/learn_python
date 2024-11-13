71434640386fe1508dfd3aa68b58d82867909be2
Avinash <avinashshiyani760@gmail.com>
2024-11-13 10:08:59 +0530

    Sorting in list in python

---
+++ SortingList_10.py
@@ -0,0 +1,9 @@
+# 10
+# Write a python program to sort the list in ascending order and descending order
+list = [1,2,80,56,-8,45]
+ascendingOrder = list.sort()
+
+print(list)
+
+descendingOrder = list.sort(reverse = True)
+print(list)
