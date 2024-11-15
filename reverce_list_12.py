0dda980fa940c2cc0a2e2268d177c0351c8ff0cc
Avinash <avinashshiyani760@gmail.com>
2024-11-15 09:47:10 +0530

    Reverce list in python]

---
+++ reverce_list_12.py
@@ -0,0 +1,10 @@
+# 12. Write a python program to concatenate two lists and then display the reverse list.
+
+listOne = ["one" , "two" , "three"]
+listTwo = ["four" , "five" , "six"]
+
+newList = listOne + listTwo
+
+newList.reverse()
+
+print(newList)
