2c370a1f4fc8dbc586d9f49ccda8bed7a76dc585
Avinash <avinashshiyani760@gmail.com>
2024-11-21 09:52:48 +0530

    conert all things in python

---
+++ convert_all_18.py
@@ -0,0 +1,15 @@
+# 18. Write a python program to convert string to list, string to tuple and string to set.
+
+string = "hello world"
+
+# convert string to list
+convertStringToList = list(string)
+print(convertStringToList)
+
+# convert string to tuple
+convertStringToTuple  = tuple(string)
+print(convertStringToTuple)
+
+# convcert string to set
+convertStringToSet = set(string)
+print(convertStringToSet)
