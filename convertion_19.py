ad1a0edceb54f07d4ca1a15c711cf9c8cd7e9bbd
Avinash <avinashshiyani760@gmail.com>
2024-11-22 17:04:05 +0530

    Again conversation ♥ in python

---
+++ convertion_19.py
@@ -0,0 +1,15 @@
+# 19. Write a python program to convert set to list, set to tuple and set to string.
+_set = { 1 , "hello" , "world"}
+
+# convert set to list
+convertSetToList = list(_set)
+print(convertSetToList)
+
+# convert set to tuple
+convertSetToTuple = tuple(_set)
+print(convertSetToTuple)
+
+# convert set to String
+convertSetToString = str(_set)
+
+print(convertSetToString)
