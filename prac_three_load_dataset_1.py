86407cd4d11f5c9462c2ce44b73eae27ce4e4d2e
Avinash <avinashshiyani760@gmail.com>
2024-11-25 08:33:53 +0530

    Load dataset using pandas liabrary

---
+++ prac_three_load_dataset_1.py
@@ -0,0 +1,10 @@
+# 2
+# Load the dataset (student_marks.csv) into a pandas DataFrame.
+# Display information: number of columns, number of records, name of all columns,
+# Also display the type of dataframe and the type of any single column.
+
+import pandas as pd
+
+df = pd.read_csv("student_marks.csv")
+
+print(df)
