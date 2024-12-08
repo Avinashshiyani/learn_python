c9981b8f2e096d71a313f61163a7d771483b901f
Avinash <avinashshiyani760@gmail.com>
2024-12-08 08:37:18 +0530

    Filter Student whose marks are less than 50

---
+++ marks_filter_less_than_15.py
@@ -0,0 +1,4 @@
+# 15
+# Display the student rows whose total marks are less than 50
+
+print(df[df["Total"] < 100])
--- sort_decending_14.py
+++ sort_decending_14.py
@@ -2,4 +2,4 @@
 # Sort all the student rows by descending order of “total” column
 
 sort_data = df.sort_values(by="Total" , ascending=False)
-print(sort_data)
+print(sort_data)a
