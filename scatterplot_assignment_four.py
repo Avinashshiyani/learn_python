84069c293a08837452ef09670bd88e63b385c9a4
Avinash <avinashshiyani760@gmail.com>
2025-01-24 08:09:38 +0530

    Scatter plot chart in assignment Four

---
+++ scatterplot_assignment_four.py
@@ -0,0 +1,6 @@
+# Scatter Plot: Maths vs Science
+plt.scatter(df['Maths'], df['Science'], color='purple')
+plt.title("Maths vs. Science Marks")
+plt.xlabel("Maths Marks")
+plt.ylabel("Science Marks")
+plt.show()
