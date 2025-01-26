1cae2f5386db778d0d106cd6bf0d588c61edee0b
Avinash <avinashshiyani760@gmail.com>
2025-01-26 17:34:28 +0530

    Area graph in python

---
+++ areagraph_assignment_four.py
@@ -0,0 +1,6 @@
+# Area Graph: Average marks in each subject by category
+avg_marks.plot(kind='area', alpha=0.9)
+plt.title("Average Marks in Each Subject by Category")
+plt.xlabel("Category")
+plt.ylabel("Marks")
+plt.show()
