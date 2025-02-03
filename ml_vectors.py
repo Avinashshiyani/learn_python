1f9cc2a5d643243534c04391d5b8d3a7cea12cac
Avinash <avinashshiyani760@gmail.com>
2025-02-03 10:38:56 +0530

    Started numpy for machine learning

---
+++ ml_vectors.py
@@ -0,0 +1,5 @@
+# 1. Create vectors
+row_vector = np.array([1, 2, 3, 4])  # Row vector
+column_vector = np.array([[5], [6], [7], [8]])  # Column vector
+print("Row vector:", row_vector)
+print("Column vector:\n", column_vector)
