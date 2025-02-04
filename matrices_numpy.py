c4bf86d63f13f01e942fc8a005062226fad3c223
Avinash <avinashshiyani760@gmail.com>
2025-02-04 15:42:05 +0530

    Matrices in numpy

---
+++ matrices_numpy.py
@@ -0,0 +1,7 @@
+# 2. Create matrices
+matrix_3x2 = np.array([[1, 2], [3, 4], [5, 6]]) 
+matrix_3x3 = np.random.randint(1, 10, (3, 3))
+matrix_4x3 = np.random.randint(1, 10, (4, 3))  
+print("3x2 matrix:\n", matrix_3x2)
+print("3x3 matrix:\n", matrix_3x3)
+print("4x3 matrix:\n", matrix_4x3)
