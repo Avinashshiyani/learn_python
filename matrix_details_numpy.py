e5ca67eb637ad0cd4575b563989876f44d93550c
Avinash <avinashshiyani760@gmail.com>
2025-02-05 08:04:45 +0530

    Matrix details in numpy in machine learning

---
+++ matrix_details_numpy.py
@@ -0,0 +1,6 @@
+# 6. Matrix details
+matrix = matrix_3x3
+print("Total elements:", matrix.size)
+print("Number of rows:", matrix.shape[0])
+print("Number of columns:", matrix.shape[1])
+print("Dimensions of the matrix:", matrix.shape)
